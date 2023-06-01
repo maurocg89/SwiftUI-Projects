81d6b233b0677295e93aec3fa7e9a1c5b5ee3e1d
maurocg89 <maurocg89@gmail.com>
2023-06-01 13:30:21 -0300

    Day 47 Challenge. Habit Tracking with TestCases

---
+++ Challenges/Day 47/HabitTracker/HabitTrackerTests/HabitTrackerTests.swift
@@ -0,0 +1,31 @@
+//
+//  HabitTrackerTests.swift
+//  HabitTrackerTests
+//
+//  Created by Mauro Grillo on 01/06/2023.
+//
+
+import XCTest
+
+final class HabitTrackerTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        // Any test you write for XCTest can be annotated as throws and async.
+        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
+        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+        let i = 0
+        
+        XCTAssert(i == 0, "I is supossed to be 0")
+    }
+
+}
