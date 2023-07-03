6204cad1047ac182e0465ed77dbace1d6c5f0a56
maurocg89 <maurocg89@gmail.com>
2023-07-03 15:55:59 -0300

    Finished day 50. CupcakeCorner part 2

---
+++ Project-10/CupcakeCorner/CupcakeCorner/Views/AddressView.swift
@@ -0,0 +1,40 @@
+//
+//  AddressView.swift
+//  CupcakeCorner
+//
+//  Created by Mauro Grillo on 03/07/2023.
+//
+
+import SwiftUI
+
+struct AddressView: View {
+    @ObservedObject var order: Order
+
+    var body: some View {
+        Form {
+            Section {
+                TextField("Name", text: $order.name)
+                TextField("Street address", text: $order.streetAddress)
+                TextField("City", text: $order.city)
+                TextField("Zip", text: $order.zip)
+            }
+
+            Section {
+                NavigationLink {
+                    CheckoutView(order: order)
+                } label: {
+                     Text("Check out")
+                }
+            }
+            .disabled(!order.hasValidAddress)
+        }
+        .navigationTitle("Delivery details")
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+struct AddressView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddressView(order: Order())
+    }
+}
