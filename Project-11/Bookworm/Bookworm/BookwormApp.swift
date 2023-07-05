8a03813f178b21525cf7e52ad74c4a7596a35868
maurocg89 <maurocg89@gmail.com>
2023-07-05 13:22:12 -0300

    Project 11. Initial commit

---
+++ Project-11/Bookworm/Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Mauro Grillo on 05/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
