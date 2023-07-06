60c7b8066abb9ed5bdc02f2e1bb4a885039d2cb8
maurocg89 <maurocg89@gmail.com>
2023-07-06 15:18:44 -0300

    Finished day 55. Project 11 Part 3 - Bookworm

---
+++ Project-11/Bookworm/Bookworm/Views/Screens/DetailView.swift
@@ -0,0 +1,70 @@
+//
+//  DetailView.swift
+//  Bookworm
+//
+//  Created by Mauro Grillo on 06/07/2023.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    @Environment(\.managedObjectContext) var moc
+    @Environment(\.dismiss) var dismiss
+
+    @State private var showingDeleteAlert = false
+
+    let book: Book
+
+    var body: some View {
+        ScrollView {
+            ZStack(alignment: .bottomTrailing) {
+                Image(book.genre ?? "Fantasy")
+                    .resizable()
+                    .scaledToFit()
+
+                Text(book.genre?.uppercased() ?? "FANTASY")
+                    .font(.caption)
+                    .fontWeight(.black)
+                    .padding(8)
+                    .foregroundColor(.white)
+                    .background(.black.opacity(0.75))
+                    .clipShape(Capsule())
+                    .offset(x: -5, y: -5)
+            }
+
+            Text(book.title ?? "Unknown Title")
+                .font(.title)
+                .foregroundColor(.secondary)
+
+            Text(book.review ?? "No review")
+                .padding()
+
+            RatingView(rating: .constant(Int(book.rating)))
+                .font(.largeTitle)
+        }
+        .navigationTitle(book.title ?? "Unknown Book")
+        .navigationBarTitleDisplayMode(.inline)
+        .alert("Delete book?", isPresented: $showingDeleteAlert) {
+            Button("Delete", role: .destructive) {
+                deleteBook()
+            }
+            Button("Cancel", role: .cancel) {}
+        } message: {
+            Text("Are you sure?")
+        }
+        .toolbar {
+            Button {
+                showingDeleteAlert = true
+            } label: {
+                Label("Delete this book", systemImage: "trash")
+            }
+        }
+    }
+
+    func deleteBook() {
+        moc.delete(book)
+
+//        try? moc.save()
+        dismiss()
+    }
+}
