035590287c24a4c089568384364509b642a79da9
maurocg89 <maurocg89@gmail.com>
2023-08-10 14:11:54 -0300

    Finished day 65. Project 13 part 4 - Instafilter

--- Project-13/Instafilter/Instafilter/ContentView.swift
+++ Project-13/Instafilter/Instafilter/ContentView.swift
@@ -5,13 +5,89 @@
 //  Created by Mauro Grillo on 28/07/2023.
 //
 
+import CoreImage
+import CoreImage.CIFilterBuiltins
 import SwiftUI
 
 struct ContentView: View {
+    @State private var image: Image?
+    @State private var filterIntensity = 0.5
+
+    @State private var showingImagePicker = false
+    @State private var inputImage: UIImage?
+
+    @State private var currentFilter = CIFilter.sepiaTone()
+    let context = CIContext()
+
     var body: some View {
-//        PropertyWrappersStructs()
-//        ConfirmationDialogPlaygroundView()
-        ImagePickerPlaygroundView()
+        NavigationView {
+            VStack {
+                ZStack {
+                    Rectangle()
+                        .fill(.secondary)
+
+                    Text("Tap to select a picture")
+                        .foregroundColor(.white)
+                        .font(.headline)
+
+                    image?
+                        .resizable()
+                        .scaledToFit()
+                }
+                .onTapGesture {
+                    showingImagePicker = true
+                }
+
+                HStack {
+                    Text("Intensity")
+                    Slider(value: $filterIntensity)
+                        .onChange(of: filterIntensity) { _ in
+                            applyProcessing()
+                        }
+                }
+                .padding(.vertical)
+
+                HStack {
+                    Button("Change Filter") {
+                        // change filter
+                    }
+
+                    Spacer()
+
+                    Button("Save", action: save)
+                }
+            }
+            .padding([.horizontal, .bottom])
+            .navigationTitle("Instafilter")
+            .sheet(isPresented: $showingImagePicker) {
+                ImagePicker(image: $inputImage)
+            }
+            .onChange(of: inputImage) { _ in
+                loadImage()
+            }
+        }
+    }
+
+    func loadImage() {
+        guard let inputImage else { return }
+        let beginImage = CIImage(image: inputImage)
+        currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
+        applyProcessing()
+    }
+
+    func save() {
+
+    }
+
+    func applyProcessing() {
+        currentFilter.intensity = Float(filterIntensity)
+
+        guard let outputImage = currentFilter.outputImage else { return }
+
+        if let cgimg = context.createCGImage(outputImage, from: outputImage.extent) {
+            let uiImage = UIImage(cgImage: cgimg)
+            image = Image(uiImage: uiImage)
+        }
     }
 }
 
---
+++ Project-13/Instafilter/Instafilter/Playground/ContentPlaygroundView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentPlaygroundView.swift
+//  Instafilter
+//
+//  Created by Mauro Grillo on 10/08/2023.
+//
+
+import SwiftUI
+
+struct ContentPlaygroundView: View {
+    var body: some View {
+//        PropertyWrappersStructs()
+//        ConfirmationDialogPlaygroundView()
+        ImagePickerPlaygroundView()
+    }
+}
+
+struct ContentPlaygroundView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentPlaygroundView()
+    }
+}
