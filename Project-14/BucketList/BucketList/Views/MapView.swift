880c925a827163a37537dc8d62c2d380a8b7162e
maurocg89 <maurocg89@gmail.com>
2023-09-04 14:49:54 -0300

    Reorganized project 14 folders. Cleaned some code

---
+++ Project-14/BucketList/BucketList/Views/MapView.swift
@@ -0,0 +1,79 @@
+//
+//  MapImprovedAnnotationsPlaygroundView.swift
+//  BucketList
+//
+//  Created by Mauro Grillo on 24/08/2023.
+//
+
+import MapKit
+import SwiftUI
+
+struct MapView: View {
+    @State private var mapRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 50, longitude: 0), span: MKCoordinateSpan(latitudeDelta: 25, longitudeDelta: 25))
+    @State private var locations = [Location]()
+
+    @State private var selectedPlace: Location?
+
+    var body: some View {
+        ZStack {
+            Map(coordinateRegion: $mapRegion, annotationItems: locations) { location in
+                MapAnnotation(coordinate: location.coordinate) {
+                    VStack {
+                        Image(systemName: "star.circle")
+                            .resizable()
+                            .foregroundColor(.red)
+                            .frame(width: 44, height: 44) // Recommended minimum size for any interactive elements in the ui
+                            .background(.white)
+                            .clipShape(Circle())
+
+                        Text(location.name)
+                            .fixedSize()
+                    }
+                    .onTapGesture {
+                        selectedPlace = location
+                    }
+                }
+            }
+            .ignoresSafeArea()
+
+            Circle()
+                .fill(.blue)
+                .opacity(0.3)
+                .frame(width: 32, height: 32)
+
+            VStack {
+                Spacer()
+
+                HStack {
+                    Spacer()
+
+                    Button {
+                        let newLocation = Location(id: UUID(), name: "New Location", description: "", latitude: mapRegion.center.latitude, longitude: mapRegion.center.longitude)
+                        locations.append(newLocation)
+                    } label: {
+                        Image(systemName: "plus")
+                    }
+                    .padding()
+                    .background(.black.opacity(0.75))
+                    .foregroundColor(.white)
+                    .font(.title)
+                    .clipShape(Circle())
+                    .padding(.trailing)
+                }
+            }
+        }
+        .sheet(item: $selectedPlace) { place in
+            LocationEditView(location: place) { newLocation in
+                if let index = locations.firstIndex(of: place) {
+                    locations[index] = newLocation
+                }
+            }
+        }
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
