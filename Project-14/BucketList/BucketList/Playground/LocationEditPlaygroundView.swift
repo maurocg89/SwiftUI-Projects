9c491e0701a8dfb0fa3c9b7c1d4414d3b1fe27bb
maurocg89 <maurocg89@gmail.com>
2023-09-04 14:43:31 -0300

    Finished day 71. Project 14 part 4 - BucketList

--- Project-14/BucketList/BucketList/Playground/LocationEditPlaygroundView.swift
+++ Project-14/BucketList/BucketList/Playground/LocationEditPlaygroundView.swift
@@ -8,6 +8,10 @@
 import SwiftUI
 
 struct LocationEditPlaygroundView: View {
+    enum LoadingState {
+        case loading, loaded, failed
+    }
+
     @Environment(\.dismiss) var dismiss
     var location: Location
     var onSave: (Location) -> Void
@@ -15,12 +19,32 @@ struct LocationEditPlaygroundView: View {
     @State private var name: String
     @State private var description: String
 
+    @State private var loadingState = LoadingState.loading
+    @State private var pages = [Page]()
+
     var body: some View {
         NavigationView {
             Form {
                 Section {
                     TextField("Place Name", text: $name)
                     TextField("Description", text: $description)
+                }
+
+                Section("Nearby...") {
+                    switch loadingState {
+                    case .loading:
+                        Text("Loading...")
+                    case .loaded:
+                        ForEach(pages, id: \.pageid) { page in
+                            /*@START_MENU_TOKEN@*/Text(page.title)/*@END_MENU_TOKEN@*/
+                                .font(.headline)
+                            + Text(": ")
+                            + Text(page.description)
+                                .italic()
+                        }
+                    case .failed:
+                        Text("Please try again later.")
+                    }
                 }
             }
             .navigationTitle("Place details")
@@ -35,6 +59,9 @@ struct LocationEditPlaygroundView: View {
                     dismiss()
                 }
             }
+            .task {
+                await fetchNearbyPlaces()
+            }
         }
     }
 
@@ -45,6 +72,26 @@ struct LocationEditPlaygroundView: View {
 
         _name = State(initialValue: location.name)
         _description = State(initialValue: location.description)
+    }
+
+    func fetchNearbyPlaces() async {
+        let urlString = "https://en.wikipedia.org/w/api.php?ggscoord=\(location.coordinate.latitude)%7C\(location.coordinate.longitude)&action=query&prop=coordinates%7Cpageimages%7Cpageterms&colimit=50&piprop=thumbnail&pithumbsize=500&pilimit=50&wbptterms=description&generator=geosearch&ggsradius=10000&ggslimit=50&format=json"
+
+        guard let url = URL(string: urlString) else {
+            print("Bad URL: \(urlString)")
+            return
+        }
+
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            let items = try JSONDecoder().decode(Result.self, from: data)
+//            pages = items.query.pages.values.sorted { $0.title < $1.title }
+            pages = items.query.pages.values.sorted()
+            loadingState = .loaded
+        } catch {
+            loadingState = .failed
+        }
+
     }
 }
 
