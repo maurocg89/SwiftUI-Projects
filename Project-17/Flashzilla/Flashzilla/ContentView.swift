fcdb3b5874855201a68ad109ccdfa8c578d6a3f5
maurocg89 <maurocg89@gmail.com>
2024-01-08 11:13:52 -0300

    Finished Day 89. Project 17 part 4 - Flashzilla
    Topics:
    
     - Coloring views as we swipe
    
     - Counting down with a Timer
    
     - Ending the app with allowsHitTesting()

--- Project-17/Flashzilla/Flashzilla/ContentView.swift
+++ Project-17/Flashzilla/Flashzilla/ContentView.swift
@@ -8,8 +8,15 @@
 import SwiftUI
 
 struct ContentView: View {
+    @Environment(\.accessibilityDifferentiateWithoutColor) var differentiateWithoutColor
+    @Environment(\.scenePhase) var scenePhase
+
     @State private var cards = Array<Card>(repeating: Card.example, count: 10)
-    
+    @State private var appIsActive = true
+    @State private var timeRemaining = 100
+
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+
     var body: some View {
         ZStack {
             Image("background")
@@ -17,6 +24,14 @@ struct ContentView: View {
                 .ignoresSafeArea()
 
             VStack {
+                Text("Time: \(timeRemaining)")
+                    .font(.largeTitle)
+                    .foregroundStyle(.white)
+                    .padding(.horizontal, 20)
+                    .padding(.vertical, 5)
+                    .background(.black.opacity(0.75))
+                    .clipShape(.capsule)
+
                 ZStack {
                     ForEach(0..<cards.count, id: \.self) { index in
                         CardView(card: cards[index]) {
@@ -27,12 +42,65 @@ struct ContentView: View {
                         .stacked(at: index, in: cards.count)
                     }
                 } // ZStack
+                .allowsHitTesting(timeRemaining > 0)
+
+                if cards.isEmpty {
+                    Button("Start Again", action: resetCards)
+                        .padding()
+                        .background(.white)
+                        .foregroundStyle(.black)
+                        .clipShape(.capsule)
+                        .padding()
+                }
+
             } // VStack
+
+            if differentiateWithoutColor {
+                VStack {
+                    Spacer()
+
+                    HStack {
+                        Image(systemName: "xmark.circle")
+                            .padding()
+                            .background(.black.opacity(0.7))
+                            .clipShape(.circle)
+                        Spacer()
+                        Image(systemName: "checkmark.circle")
+                            .padding()
+                            .background(.black.opacity(0.7))
+                            .clipShape(.circle)
+                    }
+                    .foregroundStyle(.white)
+                    .font(.largeTitle)
+                    .padding()
+                }
+            }
         } // ZStack
+        .onReceive(timer) { time in
+            guard appIsActive else { return }
+
+            if timeRemaining > 0 {
+                timeRemaining -= 1
+            }
+        }
+        .onChange(of: scenePhase) { newPhase in
+            appIsActive = newPhase == .active && !cards.isEmpty
+        }
     }
 
     func removeCard(at index: Int) {
         cards.remove(at: index)
+
+        if cards.isEmpty {
+            appIsActive = false
+        }
+
+    }
+
+    func resetCards() {
+        cards = Array(repeating: Card.example, count: 10)
+        timeRemaining = 100
+        appIsActive = true
     }
 }
 
