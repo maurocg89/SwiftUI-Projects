6d38b4408f12e35472cfc794616bad5343d0654a
maurocg89 <maurocg89@gmail.com>
2024-01-16 11:59:56 -0300

    Day 94. Project 18 Challenge - LayoutAndGeometry
     - Complete challenge 2: Make views adjust their scale depending on their vertical position

--- Project-18/LayoutAndGeometry/LayoutAndGeometry/Challenge/ChallengeContentView.swift
+++ Project-18/LayoutAndGeometry/LayoutAndGeometry/Challenge/ChallengeContentView.swift
@@ -15,13 +15,15 @@ struct ChallengeContentView: View {
                 ScrollView(.vertical) {
                     ForEach(0..<25) { index in
                         GeometryReader { geo in
-                            // For testing
+                            // For Debug
                             let maxY = geo.frame(in: .global).maxY
                             let minY = geo.frame(in: .global).minY
+                            let fullSizeHeight = fullView.size.height
                             let opacityHeight = fullView.frame(in: .global).minY + 200
                             let opacityValue = maxY <= opacityHeight ? Double(abs((opacityHeight - maxY - 200) * 0.001)) : 1
-                            
-                            let _ = print("Index: \(index), Geo MAXY: \(maxY), Geo MINY: \(minY), FullView MAXY: \(fullView.frame(in: .global).maxY), FullView MINY: \(fullView.frame(in: .global).minY), OpacityHEIGHT: \(opacityHeight), Opacity: \(geo.frame(in: .global).minY / 200), OpacityValue: \(opacityValue)")
+                            let scaleEffectValue = max((minY - fullSizeHeight / 14) / (fullView.size.height / 2), 0.5)
+
+                            let _ = debugPrint("Index: \(index), Geo MAXY: \(maxY), Geo MINY: \(minY), FullView MAXY: \(fullView.frame(in: .global).maxY), FullView MINY: \(fullView.frame(in: .global).minY), FullView Height: \(fullSizeHeight), OpacityHEIGHT: \(opacityHeight), Opacity: \(geo.frame(in: .global).minY / 200), OpacityValue: \(opacityValue), SCALE: \(scaleEffectValue)")
 
                             Text("Row #\(index)")
                                 .font(.title)
@@ -29,6 +31,7 @@ struct ChallengeContentView: View {
                                 .background(colors[index % 7]
                                     .opacity(geo.frame(in: .global).minY / 200))
                                 .rotation3DEffect(.degrees(geo.frame(in: .global).minY - fullView.size.height / 2) / 5, axis: (x: 0, y: 1, z: 0))
+                                .scaleEffect(scaleEffectValue)
                         }
                         .frame(height: 40)
                     }
