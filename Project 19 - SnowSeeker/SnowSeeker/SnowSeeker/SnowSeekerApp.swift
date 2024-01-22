bdbb914d2bc2c6e7c1d9504fe9a6e108c8d4fb56
maurocg89 <maurocg89@gmail.com>
2024-01-22 16:21:33 -0300

    Day 96. Project 19 - SnowSeeker
     - Project creation

---
+++ Project 19 - SnowSeeker/SnowSeeker/SnowSeeker/SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerApp.swift
+//  SnowSeeker
+//
+//  Created by Mauro Grillo on 22/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
