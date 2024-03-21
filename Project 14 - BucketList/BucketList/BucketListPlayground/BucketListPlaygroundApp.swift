a5eca9521aea87e464ba57c2208810dbb3dff8ce
maurocg89 <maurocg89@gmail.com>
2024-03-21 14:20:39 -0300

    Refactor Project 14 - BucketList: Add a target for playground views

---
+++ Project 14 - BucketList/BucketList/BucketListPlayground/BucketListPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListPlaygroundApp.swift
+//  BucketListPlayground
+//
+//  Created by Mauro Grillo on 21/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct BucketListPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
