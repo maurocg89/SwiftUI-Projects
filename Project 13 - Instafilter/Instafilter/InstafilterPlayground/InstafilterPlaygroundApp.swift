c212a9cef84b33fde1add7719ecc8368ddb366b8
maurocg89 <maurocg89@gmail.com>
2024-03-21 14:00:22 -0300

    Refactor Project 13 - Instafilter: Add a target for legacy code (iOS 16.4)

---
+++ Project 13 - Instafilter/Instafilter/InstafilterPlayground/InstafilterPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterPlaygroundApp.swift
+//  InstafilterPlayground
+//
+//  Created by Mauro Grillo on 21/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
