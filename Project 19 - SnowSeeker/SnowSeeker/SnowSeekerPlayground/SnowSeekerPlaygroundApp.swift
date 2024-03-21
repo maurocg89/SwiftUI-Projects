8f88a182bd156c119a7a7baf046b8eb4630b101b
maurocg89 <maurocg89@gmail.com>
2024-03-21 15:13:15 -0300

    Refactor Project 19 - SnowSeeker: Add a target for playground views

---
+++ Project 19 - SnowSeeker/SnowSeeker/SnowSeekerPlayground/SnowSeekerPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerPlaygroundApp.swift
+//  SnowSeekerPlayground
+//
+//  Created by Mauro Grillo on 21/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
